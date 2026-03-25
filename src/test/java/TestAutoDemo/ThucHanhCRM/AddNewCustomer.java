899dca5f0102c9ede32a107c0495acd4914c2841
manhh0901 <vothitramanh0901@gmail.com>
2026-03-25 17:29:17 +0700

    Update file Locator CRM

--- src/test/java/TestAutoDemo/ThucHanhCRM/AddNewCustomer.java
+++ src/test/java/TestAutoDemo/ThucHanhCRM/AddNewCustomer.java
@@ -7,17 +7,24 @@ import org.openqa.selenium.By;
 import java.sql.Driver;
 
 public class AddNewCustomer extends BaseTest {
-    public static void loginCRM(){
+    public static void loginCRM() throws InterruptedException {
         driver.get(LocatorsCRM.url);
         driver.findElement(By.xpath(LocatorsCRM.inputEmail)).sendKeys("admin@example.com");
         driver.findElement(By.xpath(LocatorsCRM.inputPwd)).sendKeys("123456");
+        Thread.sleep(1000);
         driver.findElement(By.xpath(LocatorsCRM.inputPwd)).submit();
+
+    }
+
+    public static void AddNewCustomer() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(By.xpath(LocatorsCRM.menuCustomer)).click();
 
     }
 
     public static void main(String[] args) throws InterruptedException {
         CreateDriver();
+        loginCRM();
+        AddNewCustomer();
     }
-
-
 }
